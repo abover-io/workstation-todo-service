2a7e16ade1cafb157524c2f360ab80c903c62ac1
rafiandria23 <rafiandria23@gmail.com>
2020-05-05 05:43:44 +0700

    Added user update method in generateUserTokens function.

--- src/helpers/generateUserTokens.ts
+++ src/helpers/generateUserTokens.ts
@@ -1,6 +1,6 @@
 import { sign as signJWT } from 'jsonwebtoken';
 
-import { IUserModel } from "../models/user";
+import User, { IUserModel } from "../models/user";
 
 export default async function generateUserTokens(userData: IUserModel | any): Promise<any> {
   const JWT_ACCESS_SECRET: string | any = process.env.JWT_ACCESS_SECRET;
@@ -12,6 +12,11 @@ export default async function generateUserTokens(userData: IUserModel | any): Pr
     const { firstName, lastName, username, email }: IUserModel = userData;
     const accessToken: string = await signJWT({ firstName, lastName, username, email }, JWT_ACCESS_SECRET, { expiresIn: "7d" });
     const refreshToken: string = await signJWT({ username }, JWT_REFRESH_SECRET, { expiresIn: "365d" });
+    const foundUser: IUserModel | any = await User.findOne({ username: userData.username });
+    const newRefreshTokens: Array<string> = foundUser.refreshTokens.concat(refreshToken);
+
+    await User.updateOne({ username: userData.username }, { refreshTokens: newRefreshTokens });
+
     return Promise.resolve({
       accessToken,
       refreshToken

0abb6526efd049ff46ee944f46d2fbcba8d0c1e6
rafiandria23 <rafiandria23@gmail.com>
2020-08-18 17:50:36 +0700

    UPDATE: Used timestamps option for Schemas.

--- src/models/todo-model.ts
+++ src/models/todo-model.ts
@@ -2,43 +2,36 @@ import { Schema, model, Model } from 'mongoose';
 
 import { ITodo } from '@/types';
 
-const TodoSchema: Schema = new Schema({
-  username: {
-    type: String,
-    required: [true, 'Todo has to have username!']
+const TodoSchema: Schema = new Schema(
+  {
+    username: {
+      type: String,
+      required: [true, 'Todo has to have username!'],
+    },
+    name: {
+      type: String,
+      required: [true, 'Todo name cannot be empty!'],
+    },
+    due: {
+      type: Date,
+      default: null,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    priority: {
+      type: Number,
+      default: 0,
+    },
+    position: {
+      type: Number,
+      default: null,
+    },
   },
-  name: {
-    type: String,
-    required: [true, 'Todo name cannot be empty!']
-  },
-  due: {
-    type: Date,
-    default: null
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  priority: {
-    type: Number,
-    default: 0
-  },
-  position: {
-    type: Number,
-    default: null
-  }
-});
+  { timestamps: true },
+);
 
-TodoSchema.pre('save', function (this: ITodo, next: any) {
-  this.createdAt = new Date();
-  next();
-});
-
-TodoSchema.pre('update', function (this: ITodo, next: any) {
-  this.updatedAt = new Date();
-  next();
-})
-
-const Todo: Model <ITodo> = model<ITodo>('Todo', TodoSchema);
+const Todo: Model<ITodo> = model<ITodo>('Todo', TodoSchema);
 
 export default Todo;

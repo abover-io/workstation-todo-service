ddcda0a1fb06c0b16371a305b5df01a8e5279e59
Adam Rafiandri <rafiandria23@gmail.com>
2020-11-22 10:02:35 +0700

    ADD: Added redis client.

--- src/config.ts
+++ src/config.ts
@@ -1,26 +1,26 @@
-import { getEnvVar } from './utils';
+import { getEnv } from './utils';
 
 export const apiVersion = 'v1';
 
-export const NODE_ENV: string = getEnvVar('NODE_ENV');
+export const NODE_ENV: string = getEnv('NODE_ENV');
 
-export const API_PORT: number = getEnvVar('PORT');
+export const API_PORT: number = getEnv('PORT');
 
-export const MONGODB_URI: string = getEnvVar('MONGODB_URI');
+export const MONGODB_URI: string = getEnv('MONGODB_URI');
 
-export const MONGODB_CONTAINER: string = getEnvVar('MONGODB_CONTAINER');
+export const MONGODB_CONTAINER: string = getEnv('MONGODB_CONTAINER');
 
 export const defaultTestPort: number | any =
-  getEnvVar('DEFAULT_TEST_PORT') || 4000;
+  getEnv('DEFAULT_TEST_PORT') || 4000;
 
-export const COOKIE_SECRET: string = getEnvVar('COOKIE_SECRET');
+export const COOKIE_SECRET: string = getEnv('COOKIE_SECRET');
 
-export const JWT_ACCESS_SECRET: string = getEnvVar('JWT_ACCESS_SECRET');
+export const JWT_ACCESS_SECRET: string = getEnv('JWT_ACCESS_SECRET');
 
-export const JWT_REFRESH_SECRET: string = getEnvVar('JWT_REFRESH_SECRET');
+export const JWT_REFRESH_SECRET: string = getEnv('JWT_REFRESH_SECRET');
 
-export const JWT_API_KEY_SECRET: string = getEnvVar('JWT_API_KEY_SECRET');
+export const JWT_API_KEY_SECRET: string = getEnv('JWT_API_KEY_SECRET');
 
-export const GOOGLE_OAUTH_CLIENT_ID: string = getEnvVar('GOOGLE_OAUTH_CLIENT_ID');
+export const GOOGLE_OAUTH_CLIENT_ID: string = getEnv('GOOGLE_OAUTH_CLIENT_ID');
 
-export const GOOGLE_OAUTH_CLIENT_SECRET: string = getEnvVar('GOOGLE_OAUTH_CLIENT_SECRET');
+export const GOOGLE_OAUTH_CLIENT_SECRET: string = getEnv('GOOGLE_OAUTH_CLIENT_SECRET');

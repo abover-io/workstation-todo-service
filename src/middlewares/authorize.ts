c9ab0aa9eeb91aafe98cd298668daec93e848723
Adam Rafiandri <rafiandria23@gmail.com>
2021-05-01 20:13:30 +0700

    Revised Authorize.

--- src/middlewares/authorize.ts
+++ src/middlewares/authorize.ts
@@ -1,7 +1,6 @@
 import createError from 'http-errors';
 import { Types } from 'mongoose';
 import { Request, Response, NextFunction } from 'express';
-import { verify as verifyJWT } from 'jsonwebtoken';
 
 // Types
 import { ICustomRequest } from '@/types';
@@ -38,52 +37,28 @@ export default class Authorize {
   static async Todo(req: Request, res: Response, next: NextFunction) {
     try {
       const { email }: IUser = (<ICustomRequest>req).user;
-      const listId: string | any = req.body.listId || req.query.listId;
-      const todoId: string | any =
-        req.params.todoId || req.query.todoId || req.body._id;
+      const _id: string | any =
+        req.params.todoId || req.body._id || req.query.todoId;
 
-      const foundList: IListDocument | null = await List.findOne({
-        $and: [
-          {
-            _id: Types.ObjectId(listId),
-          },
-          {
-            email,
-          },
-        ],
+      const foundLists: IListDocument[] = await List.find({
+        email,
       });
 
-      if (!foundList) {
-        throw createError(404, `List with ID ${listId} is not found!`);
-      }
+      const listIds: Types.ObjectId[] = foundLists.map((list) => list._id);
 
       const foundTodo: ITodoDocument | null = await Todo.findOne({
-        $and: [
-          {
-            _id: Types.ObjectId(todoId),
-          },
-          {
-            listId: Types.ObjectId(listId),
-          },
-        ],
+        _id: Types.ObjectId(_id),
       });
 
       if (!foundTodo) {
-        throw createError(404, `Todo with ID ${todoId} is not found!`);
-      } else if (!foundTodo.listId.equals(Types.ObjectId(listId))) {
+        throw createError(404, `Todo with ID ${_id} is not found!`);
+      } else if (!listIds.includes(foundTodo._id)) {
         throw createError(401, `You are not authorized!`);
       } else {
         return next();
       }
     } catch (err) {
-      if (
-        err.message ==
-        'Argument passed in must be a single String of 12 bytes or a string of 24 hex characters'
-      ) {
-        return next();
-      } else {
-        return next(err);
-      }
+      return next(err);
     }
   }
 
@@ -94,55 +69,28 @@ export default class Authorize {
   ): Promise<Response<any> | any> {
     try {
       const { email }: IUser = (<ICustomRequest>req).user;
-      const listId: string | any = req.query.listId;
-      const todoId: string | any = req.body.todoId || req.query.todoId;
-      const subtodoId: string | any =
+      const _id: string | any =
         req.params.subtodoId || req.body._id || req.query.subtodoId;
 
-      const foundList: IListDocument | null = await List.findOne({
-        $and: [
-          {
-            _id: Types.ObjectId(listId),
-          },
-          {
-            email,
-          },
-        ],
+      const foundLists: IListDocument[] = await List.find({
+        email,
       });
 
-      if (!foundList) {
-        throw createError(404, `List with ID ${listId} is not found!`);
-      }
-
-      const foundTodo: ITodoDocument | null = await Todo.findOne({
-        $and: [
-          {
-            _id: Types.ObjectId(todoId),
-          },
-          {
-            listId: foundList._id,
-          },
-        ],
+      const foundTodos: ITodoDocument[] = await Todo.find({
+        listId: {
+          $in: foundLists.map((list) => list._id),
+        },
       });
 
-      if (!foundTodo) {
-        throw createError(404, `Todo with ID ${todoId} is not found!`);
-      }
+      const todoIds: Types.ObjectId[] = foundTodos.map((todo) => todo._id);
 
       const foundSubtodo: ISubtodoDocument | null = await Subtodo.findOne({
-        $and: [
-          {
-            _id: Types.ObjectId(subtodoId),
-          },
-          {
-            todoId: Types.ObjectId(todoId),
-          },
-        ],
+        _id: Types.ObjectId(_id),
       });
 
       if (!foundSubtodo) {
-        throw createError(404, `Subtodo with ID ${subtodoId} is not found!`);
-      } else if (!foundSubtodo.todoId.equals(Types.ObjectId(todoId))) {
+        throw createError(404, `Subtodo with ID ${_id} is not found!`);
+      } else if (!todoIds.includes(foundSubtodo._id)) {
         throw createError(401, 'You are not authorized!');
       } else {
         return next();

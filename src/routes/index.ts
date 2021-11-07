a9f0b0783f05e50ffac27e2067f79ecb53447515
Adam Rafiandri <rafiandria23@gmail.com>
2021-11-07 12:01:16 +0700

    Renamed Fancy to Sunday.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -16,7 +16,7 @@ const MainRouter = Router();
 
 MainRouter.get('/', (_, res) => {
   return res.status(200).json({
-    service: 'Fancy Todo',
+    service: 'Sunday Todo API',
     date: moment().format('YYYY-MM-DD'),
     time: moment().format('HH:mm:ss'),
   });
